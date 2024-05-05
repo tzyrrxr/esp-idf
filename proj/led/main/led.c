552a709ee7be6f1b38e21d2e57b975aa8f6b278a
tzyrrxr <sjlin0827@outlook.com>
2024-05-05 15:05:26 +0800

    GPIO 23 led test

--- proj/led/main/led.c
+++ proj/led/main/led.c
@@ -1,21 +1,26 @@
-#include "driver/gpio.h"
 #include <stdio.h>
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 void blink() {
- gpio_pad_select_gpio(23);
  gpio_set_direction (23, GPIO_MODE_OUTPUT);
 
  while(1) {
+  ESP_LOGI("LOG", "OFF");
   gpio_set_level(23, 0);
-  vTaskDelay(1000/portTICK_RATE_MS);
+  vTaskDelay(1000/portTICK_PERIOD_MS);
+  ESP_LOGI("LOG", "ON");
   gpio_set_level(23, 1);
-  vTaskDelay(1000/portTICK_RATE_MS);
+  vTaskDelay(1000/portTICK_PERIOD_MS);
  }
+
 }
 
 void app_main(void)
 {
+
  blink();
+
 }
